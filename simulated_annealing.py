7b58cb141b68f997f18f5a402b37acb17f58ef55
Amna El-Saqa <amnahossam343@gmail.com>
2023-11-24 20:15:21 +0200

    Added initial_placement function

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -2,8 +2,8 @@ import numpy as np
 import random
 
 
-'''Function to parse the input file netlist file and return all needed info:
-(number of components, number of nets, the placement grid, and the nets connections details.)'''
+#Function to parse the input file netlist file and return all needed info:
+#(number of components, number of nets, the placement grid, and the nets connections details.)
 def parse_netlist(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -13,3 +13,17 @@ def parse_netlist(filename):
     for line in lines[1:]:
         nets.append(list(map(int,line.split(()))))
     return num_cells, num_nets, rows, cols, nets
+
+#Function for random initial placement of cells.
+def initial_placement(num_cells, rows, cols):
+    grid = np.full((rows, cols), -1)  # -1 indicates an empty cell
+    cell_positions = np.full((num_cells, 2), -1)  # New structure for cell_positions
+
+    for cell_id in range(num_cells):
+        while True:
+            x, y = np.random.randint(rows), np.random.randint(cols)
+            if grid[x, y] == -1:
+                grid[x, y] = cell_id
+                cell_positions[cell_id] = [x, y]
+                break
+    return grid, cell_positions
